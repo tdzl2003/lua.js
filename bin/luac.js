9b20b7fa93dd33a5c5a891a6cdf9f2812a714fef
tdzl2003 <tdzl2003@gmail.com>
2016-06-03 22:52:41 +0800

    solve part 1 for #4

--- bin/luac.js
+++ bin/luac.js
@@ -1,4 +1,8 @@
-/**
- * Created by Yun on 2014/9/23.
- */
+var luajs = require('../lib');
+
+var fs = require('fs');
+var fn = process.argv[2];
+var scriptContent = fs.readFileSync(fn, 'utf-8');
+
+console.log(luajs.compile(scriptContent));
 
