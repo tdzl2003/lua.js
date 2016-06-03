f2ff1c8a4b32e74b314604a11dc71f4b4a9cda4d
tdzl2003 <tdzl2003@gmail.com>
2016-06-03 21:47:39 +0800

    fix #19

--- bin/lua.js
+++ bin/lua.js
@@ -1,3 +1,10 @@
-/**
- * Created by Yun on 2014/9/23.
- */
+var luajs = require('../lib');
+
+var fs = require('fs');
+var fn = process.argv[2];
+var scriptContent = fs.readFileSync(fn, 'utf-8');
+
+var L = luajs.newContext();
+L.loadStdLib();
+L.loadString(scriptContent)();
+
